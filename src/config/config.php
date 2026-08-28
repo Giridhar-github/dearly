c44778acea136675941e6bb9ca06bcca94bae4a8
giridhar1004 <giridhar.m@commerceforz.com>
2026-08-28 15:27:15 +0530

    base_url updated

--- src/config/config.php
+++ src/config/config.php
@@ -3,21 +3,19 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+
 require_once __DIR__ . '/../../vendor/autoload.php';
-
-
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
 
 define('APP_NAME', 'Dearly');
-define('BASE_URL', 'http://localhost/dearly/public/');
+define('BASE_URL', $_ENV['BASE_URL']);
+
 define('EMAIL', 'dearly@gmail.com');
 define('PHONE', '+9111545445454');
 define('ADDRESS', 'Kochi, Kerala, India');
 
-
 date_default_timezone_set('Asia/Kolkata');
-
 
 define('DIARIES_DELIVERED', '1');
 define('AVERAGE_RATING', '4.9');
